784196cb506fe49b1cfc62724ee81448796d3de8
Sequenta <tranquilized.mind@gmail.com>
2013-09-18 11:54:57 +0600

    Добавлен слой работы с базой данных

---
+++ ToDoList/ToDoList.DataAccess/RavenConnector.cs
@@ -0,0 +1,34 @@
+﻿using Raven.Client.Document;
+using ToDoList.Domain;
+
+namespace ToDoList.DataAccess
+{
+    public class RavenConnector
+    {
+        private DocumentStore store;
+        private const string DatabaseName = "tasks";
+        public RavenConnector(string connectionString)
+        {
+            store = new DocumentStore { Url = connectionString };
+            store.Initialize();
+        }
+
+        public void Save(Task task)
+        {
+            using (var session = store.OpenSession(DatabaseName))
+            {
+                session.Store(task);
+                session.SaveChanges();
+            }
+        }
+
+        public void Delete(Task task)
+        {
+            using (var session = store.OpenSession(DatabaseName))
+            {
+                session.Delete(task);
+                session.SaveChanges();
+            }
+        }
+    }
+}

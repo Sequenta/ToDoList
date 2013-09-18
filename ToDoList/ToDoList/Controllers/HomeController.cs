a6f0ee2462e9aeb22d595b6ff9c170ec71c60738
Sequenta <tranquilized.mind@gmail.com>
2013-09-18 11:56:26 +0600

    Добавлен метод контроллера для сохранения карточки

--- ToDoList/ToDoList/Controllers/HomeController.cs
+++ ToDoList/ToDoList/Controllers/HomeController.cs
@@ -1,16 +1,36 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+using ToDoList.DataAccess;
+using ToDoList.Domain;
 
 namespace ToDoList.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
+        private RavenConnector databaseConnector;
+
+        public HomeController()
+        {
+            databaseConnector = new RavenConnector(ConfigurationManager.ConnectionStrings["RavenDB"].ConnectionString);
+        }
 
         public ActionResult Index()
         {
             return View();
         }
 
+        public JsonResult SaveTask(string taskText)
+        {
+            var task = new Task() {Message = taskText};
+            databaseConnector.Save(task);
+            var jsonResult = new Dictionary<string, string>
+            {
+                {"id", task.Id}, 
+                {"text", task.Message}
+            };
+            return Json(jsonResult, JsonRequestBehavior.AllowGet);
+        }
     }
 }
